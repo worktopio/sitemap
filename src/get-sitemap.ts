23096f3ae2d3815e511252ac852f700578117ffa
Austin Langdon <alangdon93@gmail.com>
2023-06-17 17:49:36 +0700

    fix bug

--- src/get-sitemap.ts
+++ src/get-sitemap.ts
@@ -2,7 +2,7 @@ import fs from 'fs-extra';
 import path from 'path';
 
 export default async function getSitemap() {
-  const sitemapPath = path.join(process.cwd(), '.worktop', 'pages.json');
+  const sitemapPath = path.join(process.cwd(), '.worktop', 'sitemap.json');
   const sitemap = await fs.readJson(sitemapPath);
 
   return sitemap;
